55110318bf19aac6527538749f6d8b24c68e4f97
JuanR <jd.ruedaa@uniandes.edu.co>
2021-03-18 13:01:01 -0500

    Modelo producto edit

--- mercadoOrganico/mercadoOrganicosApp/models.py
+++ mercadoOrganico/mercadoOrganicosApp/models.py
@@ -3,13 +3,18 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Producto(models.Model):
-    nombre = models.CharField(max_length=100)
-
-
 class Catalogo(models.Model):
     fecha_creacion = models.DateField(verbose_name='Fecha de creacion')
     admin_creador = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
     class Meta:
         verbose_name_plural = "Catalogos"
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    habilitado = models.BooleanField()
+    imagen = models.URLField()
+    catalogo = models.ForeignKey(to=Catalogo, onDelete=models.DO_NOTHING)
+
+    class Meta:
+        verbose_name_plural = "Productos"
